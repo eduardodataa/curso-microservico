7982d0d0dba475088327e88deebac40c93ff2d77
Eduardo Cordeiro <duducordeiro_@hotmail.com>
2021-01-13 19:09:59 -0300

    create hr-oauth project

---
+++ hr-oauth/src/test/java/com/eduardo/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
